7e083958a6a0142f2bc753fb8901554c4136d8ef
KristapsCiguzis <73535411+KristapsCiguzis@users.noreply.github.com>
2020-12-06 18:09:26 +0200

    Praktiskais_3

--- App.js
+++ App.js
@@ -1,34 +1,20 @@
-import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './screens/HomeScreen';
+import DetailsScreen from './screens/DetailsScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Image source={{ uri: "https://upload.wikimedia.org/wikipedia/commons/b/b6/Vidzemes_Augstskola_logo.png" }} style={{ width: 100, height: 100  }} />
-    
-      <Text style={{color: '#888', fontSize: 18}}> 
-        Hello World from Group 10
-      </Text>
-       <Text style={{color: '#888', fontSize: 18}}> 
-Member Kristaps Ciguzis!       
-      </Text>
-      <Text style={{color: '#888', fontSize: 17}}> 
-              
- This is my Kristaps Ciguzis first React Native application!
 
-      </Text>
-      <Image source={{ uri: "https://cdn2.iconfinder.com/data/icons/school-flat-circle/512/Boy_child_school_boy_student-512.png" }} style={{ width: 100, height: 100 }} />
-      <View>Kristaps Ciguzis</View>
-    </View>
-    
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Details" component={DetailsScreen} options={{ title: 'Details information' }}/>
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home screen' }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
